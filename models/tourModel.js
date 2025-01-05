fdb803674a369224ff67f49b8aff719149e89df6
Bhzd <bhzd.bakhshayesh@gmail.com>
2025-01-05 12:27:57 +0330

    query middleware

--- models/tourModel.js
+++ models/tourModel.js
@@ -54,7 +54,11 @@ const tourSchema = new mongoose.Schema({
         default: Date.now(),
         select: false
     },
-    startDates: [Date]
+    startDates: [Date],
+    secretTour: {
+        type: Boolean,
+        default: false
+    }
 }, { toJSON: { virtuals: true }, toObject: { virtuals: true }, })
 
 tourSchema.virtual('durationWeeks').get(function () {
@@ -73,6 +77,27 @@ tourSchema.pre('save', function (next) {
 //     console.log({ doc });
 //     next()
 // })
+
+tourSchema.pre(/^find/, function (next) {
+    this.find({ secretTour: { $ne: true } })
+    this.start = Date.now()
+    next()
+})
+
+tourSchema.post(/^find/, function (doc, next) {
+    console.log(`Query took  ${Date.now() - this.start} milliseconds`);
+    console.log(doc);
+    next()
+})
+// tourSchema.pre('find', function (next) {
+//     this.find({ secretTour: { $ne: true } })
+//     next()
+// })
+// tourSchema.pre('findOne', function (next) {
+//     this.find({ secretTour: { $ne: true } })
+//     next()
+// })
+
 
 const Tour = mongoose.model("Tour", tourSchema)
 
