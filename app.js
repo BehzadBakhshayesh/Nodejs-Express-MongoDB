b9ee9e37a437dfc2f0cb714a2441a305a740fe0a
Bhzd <bhzd.bakhshayesh@gmail.com>
2025-02-28 15:32:29 +0330

    http heaedr by helmet

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const morgan = require('morgan');
 const rateLimit = require('express-rate-limit');
+const helmet = require('helmet');
 
 const AppError = require('./utils/appError');
 const globalErrorHandler = require('./controllers/errorController');
@@ -9,40 +10,41 @@ const tourRouter = require('./routes/tourRouter');
 
 const app = express();
 
+// 1) GLOBAL MIDDLEWARES
+// Set security HTTP headers
+app.use(helmet());
+
+// Development logging
 if (process.env.NODE_ENV === 'development') {
     app.use(morgan('dev'));
 }
-const limiter = rateLimit({
-    limit: 100,
-    windowMs: 60 * 60 * 1000,
-    message: "Too many requests from this IP, please try again in an hour!"
-})
-app.use('/api', limiter)
 
-app.use(express.json());
+// Limit requests from same API
+const limiter = rateLimit({
+    max: 100,
+    windowMs: 60 * 60 * 1000,
+    message: 'Too many requests from this IP, please try again in an hour!'
+})
+app.use('/api', limiter);
+// Body parser, reading data from body into req.body
+app.use(express.json({ limit: '10kb' }));
+
+// Serving static files
 app.use(express.static(`${__dirname}/public`));
 
+// Test middleware
 app.use((req, res, next) => {
     req.requestTime = new Date().toISOString();
+    // console.log(req.headers);
     next();
 });
 
+// 3) ROUTES
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
 
 app.all('*', (req, res, next) => {
-    // res.status(404).json({
-    //     status: 'fail',
-    //     message: `Can't find ${req.originalUrl} on this server!`,
-    // });
-    // ===================
-    // const err = new Error(`Can't find ${req.originalUrl} on this server!`)
-    // err.status = 'fail'
-    // err.statusCode = 404
-
-    // next(err)
-    // =====================
-    next(new AppError(`Can't find ${req.originalUrl} on this server!`, 404))
+    next(new AppError(`Can't find ${req.originalUrl} on this server!`, 404));
 });
 
 app.use(globalErrorHandler);
