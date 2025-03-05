f961da25b3ae44c6b1e20c8a8346cdf98779a7f2
Bhzd <bhzd.bakhshayesh@gmail.com>
2025-03-05 16:32:03 +0330

    .

--- controllers/authController.js
+++ controllers/authController.js
@@ -54,11 +54,9 @@ exports.login = catchAsync(async (req, res, next) => {
     }
     // 2) Check if user exists && password is correct
     const user = await User.findOne({ email }).select('+password');
-
     if (!user || !(await user.correctPassword(password, user.password))) {
         return next(new AppError('Incorrect email or password', 401));
     }
-
     // 3) If everything ok, send token to client
     createSendToken(user, 200, res);
 });
@@ -94,7 +92,7 @@ exports.protect = catchAsync(async (req, res, next) => {
     }
 
     // 4) Check if user changed password after the token was issued
-    if (currentUser.changedPasswordAfter(decoded.iat)) {
+    if (currentUser.isChangedPasswordAfterLogIn(decoded.iat)) {
         return next(
             new AppError('User recently changed password! Please log in again.', 401)
         );
--- models/userModel.js
+++ models/userModel.js
@@ -81,16 +81,11 @@ userSchema.methods.correctPassword = async function (
     return await bcrypt.compare(candidatePassword, userPassword);
 };
 
-userSchema.methods.changedPasswordAfter = function (JWTTimestamp) {
+userSchema.methods.isChangedPasswordAfterLogIn = function (JWTTimestamp) {
     if (this.passwordChangedAt) {
-        const changedTimestamp = parseInt(
-            this.passwordChangedAt.getTime() / 1000,
-            10
-        );
-
+        const changedTimestamp = parseInt(this.passwordChangedAt.getTime() / 1000, 10);
         return JWTTimestamp < changedTimestamp;
     }
-
     // False means NOT changed
     return false;
 };
