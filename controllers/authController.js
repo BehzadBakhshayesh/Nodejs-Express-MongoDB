a5454dcb868ca3412a321dc7c6bbc1077207ecc9
Bhzd <bhzd.bakhshayesh@gmail.com>
2025-03-25 12:30:06 +0430

    .

--- controllers/authController.js
+++ controllers/authController.js
@@ -14,12 +14,13 @@ const signToken = id => {
 
 const createSendToken = (user, statusCode, res) => {
     const token = signToken(user._id);
+    console.log({ expires: process.env.JWT_COOKIE_EXPIRES_IN });
 
-    // res.cookie('jwt', token, {
-    //     expires: new Date(Date.now() + process.env.JWT_COOKIE_EXPIRES_IN * 24 * 60 * 60 * 1000),
-    //     httpOnly: true,
-    //     secure: process.env.NODE_ENV === 'production'
-    // });
+    res.cookie('jwt', token, {
+        expires: new Date(Date.now() + process.env.JWT_COOKIE_EXPIRES_IN * 24 * 60 * 60 * 1000),
+        httpOnly: true,
+        secure: process.env.NODE_ENV === 'production'
+    });
 
     // Remove password from output
     user.password = undefined;
